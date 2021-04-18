e6f096ceaa65f9b796edd6674d8497b600f4f02e
Kamil Witczak <kamilgimp99@gmail.com>
2021-04-18 17:15:22 +0200

    22.4 added

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -8,7 +8,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 
@@ -20,20 +19,8 @@ public class TrelloController {
     private final TrelloClient trelloClient;
 
     @GetMapping("getTrelloBoards")
-    public void getTrelloBoards() {
-
-        List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards().stream()
-                .filter(n -> !n.getId().isEmpty())
-                .filter(n -> n.getName().contains("Kodilla"))
-                .collect(Collectors.toList());
-
-        trelloBoards.forEach(trelloBoardDto -> {
-            System.out.println(trelloBoardDto.getId() + " - " + trelloBoardDto.getName());
-            System.out.println("This board contains lists: ");
-            trelloBoardDto.getLists().forEach(trelloList -> {
-                System.out.println(trelloList.getName() + " - " + trelloList.getId() + " - " + trelloList.isClosed());
-            });
-        });
+    public List<TrelloBoardDto> getTrelloBoards() {
+        return trelloClient.getTrelloBoards();
     }
 
     @PostMapping("createTrelloCard")
--- src/main/java/com/crud/tasks/trello/client/TrelloListDto.java
+++ src/main/java/com/crud/tasks/trello/client/TrelloListDto.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
+import javax.management.ConstructorParameters;
+
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class TrelloListDto {
