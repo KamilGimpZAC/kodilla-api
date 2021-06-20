d6b040d49dcd0db5beffd4006e97fb7767e2ede9
Kamil Witczak <kamilgimp99@gmail.com>
2021-06-20 16:30:57 +0200

    32.3 added

--- src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -26,9 +26,9 @@ public class SimpleEmailService {
     public void send(final Mail mail) {
         log.info("Starting email preparation...");
         try {
-            SimpleMailMessage mailMessage = createMailMessage(mail);
+            //SimpleMailMessage mailMessage = createMailMessage(mail);
             MimeMessagePreparator mimeMessage = createMimeMessage(mail);
-            javaMailSender.send(mailMessage);
+            //javaMailSender.send(mailMessage);
             javaMailSender.send(mimeMessage);
             log.info("Email has been sent.");
         } catch (MailException e) {
